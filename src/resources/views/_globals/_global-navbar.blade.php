9c7df0a026d85498560d2b874aeb136cecfdcba4
Björn Martensen <bjoern.martensen@gmail.com>
2015-03-27 10:37:17 +0100

    improve navbar flexibility

--- src/resources/views/_globals/_global-navbar.blade.php
+++ src/resources/views/_globals/_global-navbar.blade.php
@@ -15,24 +15,11 @@
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                {{--<li><a href="{{ url('/') }}">Home</a></li>--}}
-            </ul>
-
-            <ul class="nav navbar-nav navbar-right">
-                @if (Auth::guest())
-                    {{--<li><a href="{{ url('/auth/login') }}">Login</a></li>--}}
-                    {{--<li><a href="{{ url('/auth/register') }}">Register</a></li>--}}
-                @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                           aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-                        </ul>
-                    </li>
-                @endif
-            </ul>
+            @if(View::exists('_globals._global-navbar-navigation'))
+                @include('_globals._global-navbar-navigation')
+            @else
+                @include('facebook-app::_globals._global-navbar-navigation')
+            @endif
         </div>
     </div>
 </nav>
