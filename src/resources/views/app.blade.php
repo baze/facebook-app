5127f00aaf6fef2dadc931d4d8af9c1982a46fc5
Björn Martensen <bjoern.martensen@gmail.com>
2015-04-15 10:55:19 +0200

    fix error when pageId is not set

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta http-equiv="cleartype" content="on">
 
-    @if ($pageId)
+    @if (isset($pageId))
         {!! HTML::style('css/main.css') !!}
         {!! HTML::style('css/'.$pageId.'/main.css') !!}
     @else
