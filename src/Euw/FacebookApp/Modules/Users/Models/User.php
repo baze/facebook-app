e7b7eac09a52d8334ecf49daa81edb682d7510f5
Björn Martensen <bjoern.martensen@gmail.com>
2015-04-22 10:53:23 +0200

    user is no admin if no admin array present

--- src/Euw/FacebookApp/Modules/Users/Models/User.php
+++ src/Euw/FacebookApp/Modules/Users/Models/User.php
@@ -9,79 +9,84 @@ use SammyK\LaravelFacebookSdk\SyncableGraphNodeTrait;
 
 class UserObserver {
 
-    public function creating( $model ) {
-        $context = app()->make( 'Euw\MultiTenancy\Contexts\Context' );
-        $tenant  = $context->getOrThrowException();
+	public function creating( $model ) {
+		$context = app()->make( 'Euw\MultiTenancy\Contexts\Context' );
+		$tenant  = $context->getOrThrowException();
 
-        $model->tenant_id = $tenant->id;
-    }
+		$model->tenant_id = $tenant->id;
+	}
 
 }
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
-    use Authenticatable, CanResetPassword;
-    use SyncableGraphNodeTrait {
-        firstOrNewGraphNode as traitFirstOrNewGraphNode;
-    }
+	use Authenticatable, CanResetPassword;
+	use SyncableGraphNodeTrait {
+		firstOrNewGraphNode as traitFirstOrNewGraphNode;
+	}
 
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'users';
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'users';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'fb_id',
-        'first_name',
-        'last_name',
-        'name',
-        'email',
-        'password',
-        'tenant_id'
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'fb_id',
+		'first_name',
+		'last_name',
+		'name',
+		'email',
+		'password',
+		'tenant_id'
+	];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [ 'password', 'remember_token', 'access_token' ];
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [ 'password', 'remember_token', 'access_token' ];
 
-    protected static $graph_node_field_aliases = [
-        'id' => 'fb_id',
-    ];
+	protected static $graph_node_field_aliases = [
+		'id' => 'fb_id',
+	];
 
-    public static function boot() {
-        parent::boot();
+	public static function boot() {
+		parent::boot();
 
-        static::observe( new UserObserver );
-    }
+		static::observe( new UserObserver );
+	}
 
-    public static function firstOrNewGraphNode($attributes) {
-        $context = app()->make( 'Euw\MultiTenancy\Contexts\Context' );
-        $tenant  = $context->getOrThrowException();
+	public static function firstOrNewGraphNode( $attributes ) {
+		$context = app()->make( 'Euw\MultiTenancy\Contexts\Context' );
+		$tenant  = $context->getOrThrowException();
 
-        $attributes = array_merge( $attributes, [
-            'tenant_id' => $tenant->id
-        ] );
+		$attributes = array_merge( $attributes, [
+			'tenant_id' => $tenant->id
+		] );
 
-        return static::traitFirstOrNewGraphNode($attributes );
-    }
+		return static::traitFirstOrNewGraphNode( $attributes );
+	}
 
-    public function getIsAdminAttribute() {
-        $context = app()->make( 'Euw\MultiTenancy\Contexts\Context' );
-        $tenant = $context->getOrThrowException();
+	public function getIsAdminAttribute() {
+		$context = app()->make( 'Euw\MultiTenancy\Contexts\Context' );
+		$tenant  = $context->getOrThrowException();
 
-        $admins = config('euw-facebook-app.admins.' . $tenant->fb_page_id);
+		$admins = config( 'euw-facebook-app.admins.' . $tenant->fb_page_id );
 
-        return in_array($this->fb_id, $admins);
-    }
+		if ( $admins ) {
+			return in_array( $this->fb_id, $admins );
+		}
+
+		return false;
+
+	}
 
 }
